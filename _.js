8ac23616600a59a398b36850086af9d32ef2753b
traviscult <60018617+traviscult@users.noreply.github.com>
2021-02-19 16:07:34 -0500

    created in range method and passed all tests

--- _.js
+++ _.js
@@ -3,8 +3,19 @@ const _ = {
       const lowerClampedValue = Math.max(number, lower);
       const clampedValue = Math.min(lowerClampedValue, upper);
       return clampedValue
+    },
+    inRange(number, start, end){
+      if (end === undefined){
+        end = start 
+        start = 0
+      }if (start > end){
+        let temp = end
+        end = start 
+        start = temp
+      }
+      let isInrange = start <= number && number < end
+      return isInrange;
     }
-
   };
 
 
