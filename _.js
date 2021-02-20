56df8e25c001fe885c412016397a46a8c9f00d04
traviscult <60018617+traviscult@users.noreply.github.com>
2021-02-20 17:22:31 -0500

    created dorp  method and passed all tests

--- _.js
+++ _.js
@@ -54,6 +54,13 @@ const _ = {
       undefined
       return undefined;
     },
+    drop(array, n){
+      if(n === undefined){
+        n = 1;
+      }
+      let droppedArray = array.slice(n, array.length);
+      return droppedArray
+    }
   };
 
 
