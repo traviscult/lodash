c6e2447fd96f6c7cb537287aaff942eb543c2099
traviscult <60018617+traviscult@users.noreply.github.com>
2021-02-19 19:09:12 -0500

    created invert method and passed all tests

--- _.js
+++ _.js
@@ -35,6 +35,14 @@ const _ = {
         return true;
       }return false;
     },
+    invert(object){
+      let invertedObject = {};
+      for( let key in object){
+        const originalValue = object[key];
+        invertedObject = {originalValue : key};
+      }
+      return invertedObject;
+    },
   };
 
 
